4d14b603b21cf7aab4c8751e653c3b3c249a0590
TheMachinistMind <povtherlog@gmail.com>
2026-06-29 14:41:44 +0200

    ew vision layout, community chat UI

--- Frontend/src/components/navbar/navbar.tsx
+++ Frontend/src/components/navbar/navbar.tsx
@@ -3,9 +3,9 @@ import type { JSX } from 'react'
 function Navbar({ onAccountClick }: { onAccountClick: () => void }): JSX.Element {
   return (
     <nav id="nav">
-      <a href="#">My Courses</a>
-      <a href="#">Exams</a>
-      <a href="#">Chats</a>
+      <a href="#">Playmode</a>
+      <a href="#">community</a>
+      <a href="#">Leaderboards</a>
       <a onClick={onAccountClick}>Account</a>
     </nav>
   );
